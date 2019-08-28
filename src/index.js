1b739f28eae313ebc63d051d53c16c4ef3088464
jeff.smith <jeff.smith@usaa.com>
2019-08-28 10:04:42 -0500

    added state rules notes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//problem: getting latatude is takeing some time...
 
 class App extends React.Component{
     render(){
@@ -17,4 +18,12 @@ ReactDOM.render(
     <App/>,
     document.querySelector('#root')
 );
+
+// Rules of state:
+//      Only usable iwht class components
+//      You will confuse props with state
+//      'state' is a JS object that contains data relevant to a component
+//      Update 'state' causes the component to almost instantly render
+//      State must be initialized when a component is created
+//      State can only be updated using the functioon 'setState'
 
