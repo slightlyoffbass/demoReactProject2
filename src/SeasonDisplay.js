527619fe57a965d3d5dc0877db9a6053dfa6c736
jeff.smith <jeff.smith@usaa.com>
2019-08-28 11:59:18 -0500

    add styling

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,16 @@
+import './SeasonDisplay.css';
 import React from 'react';
+
+const seasonConfig = {
+        summer:{
+            text:'lets go to the beach',
+            iconName:'sun'
+        },
+        winter:{
+            text:'It is chilly',
+            iconName:'snowflake'
+        }
+}
 
 const getSeason = (lat, month) =>{
     if (month >2 && month < 9){
@@ -10,10 +22,12 @@ const getSeason = (lat, month) =>{
 
 const SeasonDisplay = props =>{
     const season = getSeason(props.lat, new Date().getMonth());
-    console.log(season);
+    const {text,iconName} = seasonConfig[season];
     return(
-        <div>
-            Hello
+        <div className={`season-display ${season}`}>
+            <i className={`icon-left massive ${iconName} icon`}></i>
+            <h1>{text}</h1>
+            <i className={`icon-right massive ${iconName} icon`}></i>
         </div>
     );
 };
