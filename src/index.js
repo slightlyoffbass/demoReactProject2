0fffdc9895fe33f6a84f124fcc2313867456a7c0
jeff.smith <jeff.smith@usaa.com>
2019-08-28 10:12:22 -0500

    added constructor with super(), added state with propery 'lat' for latitude

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ import ReactDOM from 'react-dom';
 //problem: getting latatude is takeing some time...
 
 class App extends React.Component{
+    // first function is called, a good place to initialize state
+    constructor(props){
+        //super is a reference to the parent's constructor
+        //will produce ReferenceError: this super() hasn't been called
+        super(props);
+        // state object
+        this.state = {lat:null};
+    }    
+    
+    // React says we have to define render!!
     render(){
         // Function to determine geolocation
         window.navigator.geolocation.getCurrentPosition(
             (position)=> console.log(position),
             (err) => console.log(err)
         );
-
         return <div>Latitude: </div>
     }
 }
