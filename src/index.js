1db6eee9df23600965b7648bb67246fe9e630086
jeff.smith <jeff.smith@usaa.com>
2019-08-28 09:56:57 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
 
-    // Function to determine geolocation
-window.navigator.geolocation.getCurrentPosition(
-    (position)=> console.log(position),
-    (err) => console.log(err)
- );
+class App extends React.Component{
+    render(){
+        // Function to determine geolocation
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=> console.log(position),
+            (err) => console.log(err)
+        );
 
-return(
-    <div>Hi Theire</div>
-)
-};
-
+        return <div>Latitude: </div>
+    }
+}
 ReactDOM.render(
     <App/>,
     document.querySelector('#root')
