0f95b1318f54c4472531061feac82277301de768
jeff.smith <jeff.smith@usaa.com>
2019-08-28 10:33:26 -0500

    start new branch

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//problem: getting latatude is takeing some time...
-//
-
 class App extends React.Component{
-    // first function is called, a good place to initialize state
+
     constructor(props){
-        //super is a reference to the parent's constructor
-        //will produce ReferenceError: this super() hasn't been called
         super(props);
-        // state object with key/value pair
-        //THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT TO THIS.STATE
         this.state = {lat:null};
-        // Function to determine geolocation
         window.navigator.geolocation.getCurrentPosition(
             position=> {
-                // update set state
-                this.setState({ lat: position.coords.latitude });
+                        this.setState({ lat: position.coords.latitude });
             },
             err => console.log(err)
         );
     }    
     
-    // React says we have to define render!!
-    // render gets called all the time, do not put time consuming methods in this area
     render(){
-        return <div>Latitude:{this.state.lat} </div>
+        return <div>Latitude: {this.state.lat} </div>
     }
 }
 ReactDOM.render(
